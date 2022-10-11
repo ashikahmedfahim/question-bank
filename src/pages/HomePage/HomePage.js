274546d7436402eb5495f0709b819329a3d23600
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2022-10-12 00:02:01 +0600

    fix: quiz section redesigned

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -9,15 +9,19 @@ const Home = () => {
     return (
         <Container>
             {data.map((quiz) => (
-                <div key={quiz.id} className='quiz'>
+                <div key={quiz.id} className='quiz d-flex flex-column flex-md-row justify-content-around align-items-center'>
                     <Image src={quiz.logo} fluid />
-                    <p>{quiz.name}</p>
-                    <p>Total Questions: {quiz.total}</p>
-                    <Link to={`blogs/${quiz.id}`} >
-                        <Button>
-                            Go to quiz {quiz.id}
-                        </Button>
-                    </Link>
+                    <div className='py-2'>
+                        <p>{quiz.name}</p>
+                        <p>Total Questions: {quiz.total}</p>
+                    </div>
+                    <div>
+                        <Link to={`blogs/${quiz.id}`} >
+                            <Button>
+                                Go to quiz {quiz.id}
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
             ))}
         </Container>
