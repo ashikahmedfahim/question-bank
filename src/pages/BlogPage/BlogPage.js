d85ea94bca5a4f8bf4703a22d5c0b68a459d0e84
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2022-10-12 19:00:02 +0600

    feat: blogs added

--- src/pages/BlogPage/BlogPage.js
+++ src/pages/BlogPage/BlogPage.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import SingleBlog from '../SingleBlog/SingleBlog';
 
 const BlogPage = () => {
+    const blogs = [
+        {
+            title: "What is the purpose of using React Router",
+            points: [
+                "React Router enables client side routing",
+                "Client side routing allows your app to update the URL from a link click without making another request for another document from the server.",
+                "This enables faster user experiences",
+                "This enables more dynamic user experiences with things like animation."
+            ]
+        },
+        {
+            title: "How does Context api works",
+            points: [
+                "Context api needs to be created at first to use",
+                "Then we need to wrap the component by its provider where the context api will be accessible",
+                "Finally using useContext we can get our desired value"
+            ]
+        },
+        {
+            title: "Describe useRef hook",
+            points: [
+                "The useRef Hook allows us to get values between renders.",
+                "It can be used to get values which we will not save in state and does not require ui update",
+                "Using this we can access the DOM directly",
+                "We can access the value using by name of the variable.current"
+            ]
+        },
+    ];
+
     return (
         <Container>
-            Blog Page
+            <p className='display-4 py-5 text-center'>Read the latest blogs here...</p>
+            {blogs.map(blog => <SingleBlog blog={blog} />)}
         </Container>
     );
 };
---
+++ src/pages/SingleBlog/SingleBlog.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import './SingleBlog.css';
+
+const SingleBlog = ({ blog }) => {
+    return (
+        <Container className='blog'>
+            <p className='blog-title'>{blog.title}</p>
+            <ul className='points'>
+                {blog.points.map(point => (
+                    <li>{point}</li>
+                ))}
+            </ul>
+        </Container>
+    );
+};
+
+export default SingleBlog;
