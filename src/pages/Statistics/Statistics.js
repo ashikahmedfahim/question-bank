87056b697269c9077dac11e03884c781a2a34d45
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2022-10-12 19:36:50 +0600

    feat: rechart stat added

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
-    const { data } = useLoaderData();
+    let { data } = useLoaderData();
+
+    data = data.map(dt=> ({id: dt.id, total: dt.total}));
+    console.log(data);
+
+
     console.log(data);
     return (
-        <Container>
-            stat
+        <Container className='d-flex flex-column justify-content-center align-items-center'>
+            <p className='display-4 pb-3'>Quiz Statistics</p>
+            <LineChart width={600} height={300} data={data}>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" />
+                <XAxis dataKey="id" />
+                <YAxis dataKey="total" />
+            </LineChart>
         </Container>
     );
 };
