cf5db13c50d42b89ee6215e2ee902aae332456ce
Ashik Ahmed Fahim <ashikahmed96@gmail.com>
2022-10-12 19:50:19 +0600

    feat: responsive stat

--- src/pages/Statistics/Statistics.js
+++ src/pages/Statistics/Statistics.js
@@ -5,22 +5,21 @@ import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
     let { data } = useLoaderData();
+    data = data.map(dt => ({ id: dt.id, total: dt.total }));
 
-    data = data.map(dt=> ({id: dt.id, total: dt.total}));
-    console.log(data);
-
-
-    console.log(data);
     return (
         <Container className='d-flex flex-column justify-content-center align-items-center'>
             <p className='display-4 pb-3'>Quiz Statistics</p>
-            <LineChart width={600} height={300} data={data}>
+            <LineChart
+                width={window.innerWidth > 800 ? window.innerWidth / 2 : window.innerWidth-40}
+                height={300} data={data}
+            >
                 <Line type="monotone" dataKey="total" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" />
                 <XAxis dataKey="id" />
                 <YAxis dataKey="total" />
             </LineChart>
-        </Container>
+        </Container >
     );
 };
 
